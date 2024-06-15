958420bfd2f174925ff1f09ada642d20619fd3f7
Hung Nguyen <101444336+hungnguyennaz@users.noreply.github.com>
2024-06-15 16:20:55 +0700

    not able to bypass using permission, drop prevention also

--- src/main/java/com/sylvcraft/events/BlockPlace.java
+++ src/main/java/com/sylvcraft/events/BlockPlace.java
@@ -1,29 +1,34 @@
 package com.sylvcraft.events;
 
+import com.sylvcraft.BlockRestrict;
 import java.util.List;
-
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import com.sylvcraft.BlockRestrict;
 import org.bukkit.event.block.BlockPlaceEvent;
-
+import org.bukkit.event.player.PlayerDropItemEvent;
 
 public class BlockPlace implements Listener {
     BlockRestrict plugin;
 
     public BlockPlace(BlockRestrict instance) {
-        plugin = instance;
+        this.plugin = instance;
     }
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent e) {
-        if (e.getPlayer().hasPermission("blockrestrict.bypass")) return;
-        if (e.getPlayer().hasPermission("blockrestrict.bypass." + e.getBlockPlaced().getType().name().toLowerCase()))
-            return;
-        List<String> blocks = plugin.getConfig().getStringList("blocks");
-        if (!blocks.contains(e.getBlockPlaced().getType().name())) return;
-
-        plugin.msg("not-allowed", e.getPlayer());
-        e.setCancelled(true);
+        List<String> blocks = this.plugin.getConfig().getStringList("blocks");
+        if (blocks.contains(e.getBlockPlaced().getType().name())) {
+            this.plugin.msg("not-allowed", e.getPlayer());
+            e.setCancelled(true);
+        }
+    }
+    @EventHandler
+    public void onPlayerDropItem(PlayerDropItemEvent e) {
+        List<String> blocks = this.plugin.getConfig().getStringList("blocks");
+        String itemType = e.getItemDrop().getItemStack().getType().name();
+        if (blocks.contains(itemType)) {
+            this.plugin.msg("not-allowed", e.getPlayer());
+            e.setCancelled(true);
+        }
     }
 }
